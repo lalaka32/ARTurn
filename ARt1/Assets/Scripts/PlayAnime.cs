ea7976c7f5a8687b82962e9bf50fa8e0fc1a8f1d
Kashegor <Kashegor@users.noreply.github.com>
2018-03-18 20:01:08 +0300

    Animesonbutons

--- ARt1/Assets/Scripts/PlayAnime.cs
+++ ARt1/Assets/Scripts/PlayAnime.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class PlayAnime : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	void  
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class PlayAnime : MonoBehaviour
+{
+    public GameObject car;
+    public void MoveForward()
+    {
+        car.GetComponent<Animator>().SetBool("Isforward", true);
+    }
+    public void MoveLeft()
+    {
+        car.GetComponent<Animator>().SetBool("Isleft", true);
+    }
+    public void MoveRight()
+    {
+        car.GetComponent<Animator>().SetBool("Isright", true);
+    }
+    public void MoveReturn()
+    {
+        car.GetComponent<Animator>().SetBool("Isreturn", true);
+    }
 }
