d19eccbfa1d846b4bbd7c4eb668f9bce263d096e
Anton <lalaka32@mail.ru>
2018-03-25 21:35:57 +0300

    Anton (#40)
    * Added simple Croutine+ Enum for names of Cars

---
+++ ARt1/Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLookAt : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward* 30 *Time.deltaTime);
+    }
+}
--- ARt1/Assets/Scripts/Instantiate.cs
+++ ARt1/Assets/Scripts/Instantiate.cs
@@ -3,37 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Instantiate : MonoBehaviour {
-    public GameObject Car;
-    // Use this for initialization
-    private GameObject[] masCars = new GameObject[4];
-    void Start() {
 
-        Create();
-        SetPositions();
+    public GameObject Car;
+
+    private GameObject[] masCars = new GameObject[4];
+    NamesOfCars names;
+
+    void Start() {
+        StartCoroutine(SimpleGenerator());
         
     }
-    void Create()
+
+    
+    IEnumerator SimpleGenerator()
+    {
+        
+        InstantiateCars();
+        SetPositionsAndAngles();
+        yield return new WaitForEndOfFrame();
+    }
+
+    void InstantiateCars()
     {
         for (int i = 0; i < 4; i++)
         {
             masCars[i] = Instantiate(Car, transform, false);
-            masCars[i].transform.localScale = new Vector3(0.2000001f, 0.2f, 0.2000001f);
+            masCars[i].transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            if (i==0)
+            {
+                names = NamesOfCars.PlayerCar;
+                masCars[i].transform.name = names.ToString();
+            }
+            else
+            {
+                names = NamesOfCars.OtherCar;
+                masCars[i].transform.name = names.ToString()+i;
+            }
         }
     }
-    void SetPositions()
+
+    void SetPositionsAndAngles()
     {
-        masCars[0].transform.localPosition = new Vector3(-0.167f, 0.05449999f, 0.771f);//p2
-        masCars[0].transform.localEulerAngles = new Vector3(0, 180, 0);
+        masCars[0].transform.localPosition = new Vector3(-0.6815f, 0.0545f, -0.163f);//p1
+        masCars[0].transform.localEulerAngles = new Vector3(0, 90, 0);
 
-        masCars[1].transform.localPosition = new Vector3(0.754f, 0.05449999f, 0.128f);//p3
-        masCars[1].transform.localEulerAngles = new Vector3(0, -90, 0);
+        masCars[1].transform.localPosition = new Vector3(-0.167f, 0.0545f, 0.771f);//p2
+        masCars[1].transform.localEulerAngles = new Vector3(0, 180, 0);
 
-        masCars[2].transform.localPosition = new Vector3(0.178f, 0.05449999f, -0.712f);//p4
-        masCars[2].transform.localEulerAngles = new Vector3(0, 180, 0);
+        masCars[2].transform.localPosition = new Vector3(0.754f, 0.0545f, 0.128f);//p3
+        masCars[2].transform.localEulerAngles = new Vector3(0, 270, 0);
 
-        masCars[3].transform.localPosition = new Vector3(-0.6815f, 0.0545f, -0.163f);//p1
-        masCars[3].transform.localEulerAngles = new Vector3(0, 90, 0);
+        masCars[3].transform.localPosition = new Vector3(0.178f, 0.0545f, -0.712f);//p4    
     }
-    
-
+    enum NamesOfCars:byte { PlayerCar = 0, OtherCar}
 }
