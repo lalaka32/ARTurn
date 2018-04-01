e87fa0a56b3bffaa63a69e1957895340edfa4416
Anton <lalaka32@mail.ru>
2018-04-01 19:36:06 +0300

    наброски для логики
    * NIHYYA
    
    * LookAt
    
    * КАКОЙТА ФАЙЛ
    
    * 3
    
    * 3
    
    * sc
    
    * Added simple Croutine+ Enum for names of Cars
    
    * hz
    
    * наброски для логики

--- ARt1/Assets/Scripts/Instantiate.cs
+++ ARt1/Assets/Scripts/Instantiate.cs
@@ -6,6 +6,8 @@ public class Instantiate : MonoBehaviour
 {
     public GameObject Car;
     public GameObject[] masCars;
+    public bool trafficLight;
+
     public RuntimeAnimatorController[] contollers = new RuntimeAnimatorController[4];
     NamesOfCars names;
     private void Awake()
@@ -28,7 +30,7 @@ public class Instantiate : MonoBehaviour
         for (int i = 0; i < 4; i++)
         {
             masCars[i] = Instantiate(Car, transform, false);
-            masCars[i].transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            masCars[i].transform.localScale = new Vector3(0.165f, 0.165f, 0.165f);
             if (i == 0)
             {
                 names = NamesOfCars.PlayerCar;
@@ -46,12 +48,15 @@ public class Instantiate : MonoBehaviour
         for (int i = 0; i < masCars.Length; i++)
         {
             masCars[i].AddComponent<Animator>().runtimeAnimatorController = contollers[i];
-
+            
         }
     }
 
     void SetPositionsAndAngles()
     {
+        masCars[0].transform.localPosition = new Vector3(-0.6815f, 0.0545f, -0.163f);//p1
+        masCars[0].transform.localEulerAngles = new Vector3(0, 90, 0);
+
         masCars[1].transform.localPosition = new Vector3(-0.167f, 0.05449999f, 0.771f);//p2
         masCars[1].transform.localEulerAngles = new Vector3(0, 180, 0);
 
@@ -60,10 +65,13 @@ public class Instantiate : MonoBehaviour
 
         masCars[3].transform.localPosition = new Vector3(0.178f, 0.05449999f, -0.712f);//p4
         masCars[3].transform.localEulerAngles = new Vector3(0, 0, 0);
-
-        masCars[0].transform.localPosition = new Vector3(-0.6815f, 0.0545f, -0.163f);//p1
-        masCars[0].transform.localEulerAngles = new Vector3(0, 90, 0);
-
     }
-    enum NamesOfCars:byte { PlayerCar = 0, OtherCar}
+    void GenerationPriritets()
+    {
+        //есть ли светофоры
+        //есть ли знаки приоритета
+        //
+    }
+        
+    enum NamesOfCars : byte { PlayerCar = 0, OtherCar }
 }
---
+++ ARt1/Assets/Scripts/LOGIK_V001.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LOGIK_V001 : MonoBehaviour {
+
+    string question;
+    Dictionary<byte,GameObject> bigDick;
+    public GameObject[] masCars;
+
+    // Use this for initialization
+    void Start () {
+        masCars = GetComponent<Instantiate>().masCars;
+        // METOD V INSTANCIATE
+        
+    }
+	void InitAnsUser(byte playerPriority)
+    {
+        bigDick.Add(playerPriority, masCars[0]);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
