2bc3a9c7fb2d12c0a0f1207d4777036200e4526c
lalaka32 <lalaka32@mail.ru>
2018-04-04 10:59:14 +0300

    Generation AND POLIMORFIZMOOOO

--- ARt1/Assets/Scripts/Enums.cs
+++ ARt1/Assets/Scripts/Enums.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace Enums
 {
-    public enum NamesOfCars : byte { PlayerCar = 0, OtherCar }
     public enum TrafficLight : byte { off = 0, red, green }
     public enum Direction : byte { forward = 0, left, right, turn }
     public enum Priority : byte { first = 0, second, third, fourth }
