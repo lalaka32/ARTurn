8663229826641f9e866c777095992f1165ab9fc7
Kashegor <Kashegor@users.noreply.github.com>
2018-05-06 20:16:46 +0300

    fix logic

--- ARt1/Assets/Scripts/Instantiate.cs
+++ ARt1/Assets/Scripts/Instantiate.cs
@@ -35,7 +35,7 @@ public class Instantiate : MonoBehaviour
     }
     private void Start()
     {
-        StartCoroutine(SimpleGenerator());//измени здесь для 1-ого создания
+        StartCoroutine(SimpleGenerator());
 
     }
     IEnumerator SimpleGenerator()
